3453905580bd7a98077e63f5927cfd005ef3beaa
Ivan Ružević <ivan.ruzevic@infinum.com>
2024-07-02 23:11:45 +0200

    adding star rating

--- project/index.js
+++ project/index.js
@@ -9,28 +9,35 @@ const storageName = 'todo-list';
 // Selectors and elements.
 const ratingTemplate = document.querySelector('.js-rating-template');
 const reviewsList = document.querySelector('.js-ratings-list');
-const ratingOverview = document.querySelector('.js-rating-overview');
+const ratingOverviewValue = document.querySelector('.js-rating-overview-value');
+const ratingOverviewRating = document.querySelector('.js-rating-overview-rating');
 
 const form = document.querySelector('.js-form');
+const formRating = form.querySelector('.js-form-rating');
+const formRatingItem = form.querySelectorAll('.js-form-rating-item');
 const formText = form.querySelector('.js-form-text');
-const formValue = form.querySelector('.js-form-value');
 
 // Mock data.
 const mockReviews = [
 	{
-		title: 'Review 1',
+		content: 'Review 1',
 		rating: 1,
 	},
 	{
-		title: 'Review 2',
+		content: 'Review 2',
 		rating: 2,
 	},
 	{
-		title: 'Review 3',
+		content: 'Review 3',
 		rating: 3,
 	},
 ];
 
+/**
+ * Initialize the storage with the mock data if it doesn't exist.
+ *
+ * @returns {void}
+ */
 function initStorage() {
 	const reviews = getLocalStorage(storageName);
 
@@ -39,25 +46,60 @@ function initStorage() {
 	}
 }
 
+/**
+ * Re-render the reviews list.
+ *
+ * @param {Array} data
+ *
+ * @returns {void}
+ */
+function reRenderReviews(data) {
+	setLocalStorage(storageName, data);
+	calcOverview();
+	renderReviews();
+}
+
+/**
+ * Initialize the star rating logic.
+ *
+ * @returns {void}
+ */
+function initStarRating() {
+	formRatingItem.forEach((item) => {
+		item.addEventListener('click', (event) => {
+			formRating.setAttribute('data-rating', event.target.getAttribute('data-rate'));
+		});
+	});
+}
+
+/**
+ * Initialize the form logic.
+ *
+ * @returns {void}
+ */
 function initForm() {
 	form.addEventListener('submit', (event) => {
 		event.preventDefault();
 
-		const review = {
-			title: formText.value,
-			rating: parseInt(formValue.value, 10),
-		};
-
-		const newReviews = getLocalStorage(storageName).concat(review);
-
-		setLocalStorage(storageName, newReviews);
-		calcOverview();
-		renderReviews(newReviews);
+		reRenderReviews([
+			...getLocalStorage(storageName) ?? [],
+			{
+				content: formText.value,
+				rating: parseInt(formRating.getAttribute('data-rating'), 10),
+			}
+		]);
 
 		event.target.reset();
 	});
+
+	initStarRating();
 }
 
+/**
+ * Calculate the average rating and update the overview element.
+ *
+ * @returns {void}
+ */
 function calcOverview() {
 	let totalRating = 0;
 
@@ -65,37 +107,46 @@ function calcOverview() {
 
 	reviews.forEach((review) => totalRating += review.rating);
 
-	return ratingOverview.textContent = totalRating / reviews.length;
+	ratingOverviewValue.textContent = (reviews.length >0) ? Math.round((totalRating / reviews.length) * 100) / 100 : 0;
+	ratingOverviewRating.setAttribute('data-rating', parseInt(ratingOverviewValue.textContent, 10));
 }
 
 /**
- * Get rating template item and populate it with content.
+ * Get the review item from template.
  *
- * @param {string} content Rating content.
- * @param {int} value Rating value.
+ * @param {object} review The review object.
  *
- * @returns {Node} Rating item.
+ * @returns {Node} The review item.
  */
-function getReviewItem(content, value) {
+function getReviewItem(review) {
+	const { rating, content } = review;
+
+	if (!rating || !content) {
+		return '';
+	}
+
 	const clone = ratingTemplate.content.cloneNode(true);
 
 	const ratingItemContent = clone.querySelector('.js-rating-template-content');
 	const ratingItemValue = clone.querySelector('.js-rating-template-value');
 	const ratingItemButton = clone.querySelector('.js-rating-template-delete');
+	const ratingItemRating = clone.querySelector('.js-rating-template-rating');
 
 	ratingItemContent.textContent = content;
-	ratingItemValue.textContent = value;
+	ratingItemValue.textContent = rating;
+	ratingItemRating.setAttribute('data-rating', rating);
+
 	ratingItemButton.onclick = () => {
-		console.log('a');
+		reRenderReviews(
+			getLocalStorage(storageName).filter((t) => JSON.stringify(t) !== JSON.stringify(review))
+		)
 	};
 
 	return clone;
 }
 
 /**
- * Render reviews.
- *
- * @param {array} reviews Review list.
+ * Render the reviews list.
  *
  * @returns {void}
  */
@@ -103,8 +154,8 @@ function renderReviews() {
 	reviewsList.innerHTML = '';
 
 	getLocalStorage(storageName).forEach((review) => {
-		const item = getReviewItem(review.title, review.rating);
-		reviewsList.appendChild(item);
+		const item = getReviewItem(review);
+		reviewsList.prepend(item);
 	});
 }
 
