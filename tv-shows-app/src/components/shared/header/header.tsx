61a330a4f0f463434163a5f45284865c3fe8adab
Ivan Ružević <ivan.ruzevic@infinum.com>
2024-07-05 09:01:54 +0200

    initial project setup

---
+++ tv-shows-app/src/components/shared/header/header.tsx
@@ -0,0 +1,9 @@
+import { Button, ButtonGroup, Flex, Heading } from "@chakra-ui/react";
+
+export const Header = () => {
+	return (
+		<Flex justifyContent={'space-between'} padding={3}>
+			<Heading>Tv Shows App</Heading>
+		</Flex>
+	);
+}
